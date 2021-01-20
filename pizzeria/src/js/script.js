5e55f11eed6940e52eaccadb11db179d6be00b2e
Paweł Waśkiewicz <pawel.waskiewicz@gmail.com>
2021-01-20 09:55:45 +0100

    Add class & initMenu

--- pizzeria/src/js/script.js
+++ pizzeria/src/js/script.js
@@ -52,7 +52,43 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      Product.id = id;
+      Product.data = data;
+    
+      const thisProduct = this;
+    
+      thisProduct.renderInMenu();
+      console.log('new Product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+
+      /*create element using utils.createElementFromHTML */
+
+      
+    }
+  }
+
   const app = {
+     
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      for (let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    }, 
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +96,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
