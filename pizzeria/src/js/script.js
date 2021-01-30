0be671a78bf26e4ebdff4bfbf9adfbff436823bf
Paweł Waśkiewicz <pawel.waskiewicz@gmail.com>
2021-01-30 11:48:21 +0100

    Add images to processOrder

--- pizzeria/src/js/script.js
+++ pizzeria/src/js/script.js
@@ -89,6 +89,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -142,7 +143,7 @@
     
       // set price to default price
       let price = thisProduct.data.price;
-    
+      
       // for every category (param)...
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
@@ -154,18 +155,25 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+          // Find image .paramId-optionId in div
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId); 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)){
-            // check if the option is not default
-            if(option.default && option.defeault || option.default != true) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+              // check if the option is not default
+              if(!option.default && option.defeault || option.default !== true) {
               // add option price to price variable
-              price += option.price;
-            } 
-          } else {
-            // check if the option is default
-            if(option.default == true) {
-            // reduce price variable
-              price -= option.price;
+                price += option.price;
+              } 
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+              // check if the option is default
+              if(option.default === true) {
+                // reduce price variable
+                price -= option.price;
+              }
             }
           }
         }
